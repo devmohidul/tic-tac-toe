0bc059b778e272e902002b4d248d4752af055449
devmohidul <mohidulhasan145@gmail.com>
2025-05-26 13:20:33 +0600

    tic-tac-toe initial setup with board component initial setup

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+export default function Board() {
+  return (
+    <>
+      <div>
+        <button className="bg-white border border-gray-400 w-12 h-12 font-bold leading-9 m-1 text-2xl">
+          X
+        </button>
+        <button className="bg-white border border-gray-400 w-12 h-12 font-bold leading-9 m-1 text-2xl">
+          X
+        </button>
+        <button className="bg-white border border-gray-400 w-12 h-12 font-bold leading-9 m-1 text-2xl">
+          X
+        </button>
+      </div>
+      <div>
+        <button className="bg-white border border-gray-400 w-12 h-12 font-bold leading-9 m-1 text-2xl">
+          X
+        </button>
+        <button className="bg-white border border-gray-400 w-12 h-12 font-bold leading-9 m-1 text-2xl">
+          X
+        </button>
+        <button className="bg-white border border-gray-400 w-12 h-12 font-bold leading-9 m-1 text-2xl">
+          X
+        </button>
+      </div>
+      <div>
+        <button className="bg-white border border-gray-400 w-12 h-12 font-bold leading-9 m-1 text-2xl">
+          X
+        </button>
+        <button className="bg-white border border-gray-400 w-12 h-12 font-bold leading-9 m-1 text-2xl">
+          X
+        </button>
+        <button className="bg-white border border-gray-400 w-12 h-12 font-bold leading-9 m-1 text-2xl">
+          X
+        </button>
+      </div>
+    </>
+  );
+}
